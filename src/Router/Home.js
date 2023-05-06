c868efbc21d5e5e375bf882442c8d9d14e1115d6
dharan-dhakshan97 <dharanidhakshan97@gmail.com>
2023-05-06 18:35:07 +0530

    add initial commit

--- src/Router/Home.js
+++ src/Router/Home.js
@@ -1,19 +1,35 @@
-import React from 'react'
+import React from "react";
 import { Link } from "react-router-dom";
+// import { useState } from "react";
+
 // import About  from "../Router/About";
 const Home = () => {
   //  function getData(data) {
   //    console.log("get the Datas");
   //  }
+
+  
   return (
     <>
-   
-    <h1 style={{textAlign:'center'}}>This is Home Page </h1>
-    {/* <About handleClick={getData}/> */}
-    
-    <Link to="/about" style={{display:'flex', justifyContent:'flex-end',marginRight:'20px', color:'blue',textDecoration:'none'}}>About page</Link>
+      <h1 style={{ textAlign: "center" }}>This is Home Page </h1>
+      {/* <About handleClick={getData}/> */}
+
+     
+
+      <Link
+        to="/about"
+        style={{
+          display: "flex",
+          justifyContent: "flex-end",
+          marginRight: "20px",
+          color: "blue",
+          textDecoration: "none",
+        }}
+      >
+        About page
+      </Link>
     </>
-  )
-}
+  );
+};
 
 export default Home;
